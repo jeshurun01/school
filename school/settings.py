a09ed1dedc856c1434ffeac78549a6be8fbd6004
jeshurun <jeshurunnasser@gmail.com>
2022-08-21 22:15:23 +0200

    Le rearangement du fichier

--- school/settings.py
+++ school/settings.py
@@ -58,7 +58,9 @@ ROOT_URLCONF = "school.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            BASE_DIR / "templates"
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -120,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
